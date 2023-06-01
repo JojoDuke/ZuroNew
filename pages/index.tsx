e488ee19a68cb29f4725fb47e30505c3352e0d50
JojoDuke <45549237+JojoDuke@users.noreply.github.com>
2023-06-01 18:27:22 +0000

    coleman

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,10 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import closeIcon from '../public/close.png';
-import zuroLogo from '../public/zuro_logo.png'
+import zuroLogo from '../public/zuro_logo.png';
+import progress1 from '../public/progress1.png';
+import user from '../public/user.png';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const Home: NextPage = () => {
   return (
@@ -20,7 +23,12 @@ const Home: NextPage = () => {
         width="70px"
         height="20px"
     />
-      <p className="text-white">Progress</p>
+      <Image
+        src={progress1}
+        alt="progress1"
+        width="238px"
+        height="19px"
+    />
       <Image
         src={closeIcon}
         className="invert cursor-pointer"
@@ -35,8 +43,75 @@ const Home: NextPage = () => {
   <h3 className="text-md text-left mx-auto w-[470px]">Fill out the form below to receive a personalized insurance quote tailored to your unique needs and preferences. Our experienced team will review your information and provide you with the best coverage options available.</h3>
 
         <p className="text-left font-bold mt-3 mx-auto w-[470px]">Personal Information</p>
-      </section>
 
+         {/* Main Form */}
+        <Formik 
+            initialValues={{
+              fullName: '',
+              email: '',
+              phoneNumber: '',
+              location: '',
+            }}
+            onSubmit={(values) => {
+              alert(values)
+            }}
+          >
+          <Form>
+            {/* Fullname field */}
+            <div className="flex flex-col mt-4 w-[470px] mx-auto">
+              <Field
+                type="text"
+                id="fullName"
+                name="fullName"
+                className="bg-[#181818] border-2 border-[#4A4754] p-4 rounded-md text-white placeholder-white"
+                placeholder="Enter full name"
+              />
+              
+              <ErrorMessage name="fullName" component="div" className="text-red-500" />
+            </div>
+
+            {/* Email field */}
+            <div className="flex flex-col mt-4 w-[470px] mx-auto">
+              <Field
+                type="email"
+                id="email"
+                name="email"
+                className="bg-[#181818] border-2 border-[#4A4754] p-4 rounded-md text-white placeholder-white"
+                placeholder="Enter email address"
+              />
+              <ErrorMessage name="email" component="div" className="text-red-500" />
+            </div>
+            {/* Phone number field */}
+            <div className="flex flex-col mt-4 w-[470px] mx-auto">
+              <Field
+                type="text"
+                id="phoneNumber"
+                name="phoneNumber"
+                className="bg-[#181818] border-2 border-[#4A4754] p-4 rounded-md text-white placeholder-white"
+                placeholder="Enter phone number"
+              />
+              <ErrorMessage name="phoneNumber" component="div" className="text-red-500" />
+            </div>
+            {/* Location field */}
+            <div className="flex flex-col mt-4 w-[470px] mx-auto">
+              <Field
+                type="text"
+                id="location"
+                name="location"
+                className="bg-[#181818] border-2 border-[#4A4754] p-4 rounded-md text-white placeholder-white"
+                placeholder="Location"
+              />
+              <ErrorMessage name="Location" component="div" className="text-red-500" />
+            </div>
+          </Form>
+        </Formik>
+
+         <p className="text-left font-bold mt-16 mx-auto w-[470px]">Do you have a current provider</p>
+         <div className="flex flex-row gap-5 justify-center items-center">
+          <button>Yes</button>
+          <button>No</button>
+         </div>
+      </section>
       </main>
     </div>
   )
