361a2958a6cb33a58877d4af72e56a863b726bec
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2023-06-13 12:47:30 +0000

    get quote heading

---
+++ component/Heading.tsx
@@ -0,0 +1,11 @@
+const Heading = () => {
+    return(
+        <>
+            <h1 className="font-bold text-6xl text-center mb-4">Get a Free Quote</h1>
+            
+            <h3 className="text-md text-left mx-auto w-[470px]">Fill out the form below to receive a personalized insurance quote tailored to your unique needs and preferences. Our experienced team will review your information and provide you with the best coverage options available.</h3>
+        </>
+    )
+}
+
+export default Heading
